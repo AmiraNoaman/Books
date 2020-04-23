9f66399cfb76b6b8b7df43b4c0fb2f5509e98c4a
AmiraNoaman <you@example.com>
2020-04-23 02:32:23 +0200

    n

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,5 +3,5 @@ set -e
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
-git remote add production ssh://amira_noaman@23.96.41.241:8080/home/amira_noaman/.git/
+git remote add production ssh://amira_noaman@23.96.41.241/home/amira_noaman/Books/
 git push production master # push our updates
